7b89b7e3b5314ab7144b7fb64b9e2fa92014a3d1
Samuel Lima da Silva <samuelima.00@outlook.com>
2024-07-09 09:12:36 -0300

    Ajusta o componenente BaseInput Removido o label e deixado apenas o input no componente Criado um componente a parte para o label

---
+++ academic-calendar/src/components/BaseLabel.vue
@@ -0,0 +1,20 @@
+<template>
+    <label :for="inputId" class="form-label">
+        <slot></slot> 
+    </label>
+</template>
+
+<script>
+    import "@/assets/main_bootstrap.scss";
+    import * as bootstrap from 'bootstrap';
+
+    export default {
+        name: 'SearchInput',
+        props: {
+            inputId: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
--- academic-calendar/src/components/BaseTextInput.vue
+++ academic-calendar/src/components/BaseTextInput.vue
@@ -1,16 +1,11 @@
 <template>
-    <div>
-        <label :for="id" class="form-label" v-if="label !== '' "> {{label}} </label>
-        <input 
-            :id="id" 
-            :type="type" 
-            class="form-control" 
-            :placeholder="placeholder" 
-            :value="value"
-            @input="$emit('input', $event.target.value)"
-        >
-    </div>
-
+    <input 
+        :id="id" 
+        :type="type" 
+        class="form-control" 
+        :placeholder="placeholder" 
+        :value="value"
+        @input="$emit('input', $event.target.value)" >
 </template>
 
 <script>
@@ -24,17 +19,13 @@
                 type: String,
                 required: true
             },
-            label: {
-                type: String,
-                required: true
-            },
             id: {
                 type: String,
                 required: true
             },
             placeholder: {
                 type: String,
-                required: true
+                required: false
             },
             value: String
         },
