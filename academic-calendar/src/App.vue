d0aa42ef806a9511a42e0e72de28cd04bef89247
Samuel Lima da Silva <samuelima.00@outlook.com>
2024-07-20 20:43:09 -0300

    adiciona calendário de eventos Adiciona componente de calendário da tui

--- academic-calendar/src/App.vue
+++ academic-calendar/src/App.vue
@@ -4,23 +4,20 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div id="app">
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/calendars">Calendars</router-link>
+        <router-link to="/login">Login</router-link>
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-          <router-link to="/calendars">Calendars</router-link>
-          <router-link to="/login">Login</router-link>
-
-        </nav>
+      </nav>
+    </div>
+      <div>
+        <router-view />
       </div>
-    </header>
 
-    <router-view />
+    
   </div>
 </template>
 
@@ -28,18 +25,18 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
+  
 }
-
+nav {
+  overflow: hidden;
+  position: sticky;
+  position: -webkit-sticky;
+  top: 0;
+  margin-left: -1rem;
+}
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
@@ -58,32 +55,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- academic-calendar/src/views/CalendarDetail.vue
+++ academic-calendar/src/views/CalendarDetail.vue
@@ -1,28 +1,282 @@
 <template>
-    <div>
-    Página
+  <div>
+    <select
+      v-model="selectedView"
+      class="view-select"
+    >
+      <option
+        v-for="view in viewOptions"
+        :key="view.value"
+        :value="view.value"
+      >
+        {{ view.title }}
+      </option>
+    </select>
+    <div class="buttons">
+      <BaseButton
+        type="button"
+        @click.native="onClickTodayButton" >
+        Hoje
+      </BaseButton>
+      <BaseButton
+        type="button"
+        @click.native="onClickMoveButton(-1)" >
+         << Mês passado
+      </BaseButton>
+      <BaseButton
+        type="button"
+        @click.native="onClickMoveButton(1)">
+        Mês que vem >>
+      </BaseButton>
     </div>
+    <span class="date-range">{{ dateRangeText }}</span>
+    <ToastUICalendar
+      ref="calendar"
+      style="height: 800px;width: 1200px;"
+      :view="'month'"
+      :use-form-popup="false"
+      :use-detail-popup="false"
+      :week="{
+        showTimezoneCollapseButton: true,
+        timezonesCollapsed: false,
+        eventView: true,
+        taskView: true,
+      }"
+      :month="{ startDayOfWeek: 1 }"
+      :timezone="{ zones }"
+      :theme="theme"
+      :template="{
+        milestone: getTemplateForMilestone,
+        allday: getTemplateForAllday,
+      }"
+      :grid-selection="true"
+      :calendars="calendars"
+      :events="events"
+      @selectDateTime="onSelectDateTime"
+      @beforeCreateEvent="onBeforeCreateEvent"
+      @beforeUpdateEvent="onBeforeUpdateEvent"
+      @beforeDeleteEvent="onBeforeDeleteEvent"
+      @afterRenderEvent="onAfterRenderEvent"
+      @clickDayName="onClickDayName"
+      @clickEvent="onClickEvent"
+      @clickTimezonesCollapseBtn="onClickTimezonesCollapseBtn"
+    />
+  </div>
 </template>
-<script>
-    import axios from 'axios'
-    import { mapStores } from 'pinia'
-    import { useUserAuthInfoStore } from '@/stores/userAuthInfo'
 
-    export default {
-        methods: {
-            getEvents() {
-                axios.get(`/api/academic-calendar/calendar/${this.$route.params.id}/events`, {
-                    headers: {
-                        'Authorization': 'Bearer ' + this.userAuthInfoStore.token
-                    }
-                })
-            }
-        },
-        mounted: function() {
-            this.getEvents();
-        },
-        computed: {
-            ...mapStores(useUserAuthInfoStore)
+<script>
+/* eslint-disable no-console */
+import ToastUICalendar from '@/components/ToastUICalendar.vue';
+import '@toast-ui/calendar/toastui-calendar.css';
+
+//import { events } from '@/views/assets/mock-data.js';
+import { theme } from '@/views/assets/theme.js';
+
+import axios from 'axios'
+import { mapStores } from 'pinia'
+import { useUserAuthInfoStore } from '@/stores/userAuthInfo'
+import BaseButton from '@/components/BaseButton.vue'
+
+export default {
+  data() {
+    return {
+      calendars: [
+        {
+          id: '0',
+          name: 'Private',
+          backgroundColor: '#9e5fff',
+          borderColor: '#9e5fff',
+          dragBackgroundColor: '#9e5fff',
         }
-    }
+      ],
+      events: [],
+      zones: [
+        {
+          timezoneName: 'Asia/Seoul',
+          displayLabel: 'Seoul',
+          tooltip: 'UTC+09:00',
+        },
+        {
+          timezoneName: 'Pacific/Guam',
+          displayLabel: 'Guam',
+          tooltip: 'UTC+10:00',
+        },
+      ],
+      theme,
+      selectedView: 'month',
+      viewOptions: [
+        {
+          title: 'Monthly',
+          value: 'month',
+        },
+        {
+          title: 'Weekly',
+          value: 'week',
+        },
+        {
+          title: 'Daily',
+          value: 'day',
+        },
+      ],
+      dateRangeText: '',
+    };
+  },
+  computed: {
+    calendarInstance() {
+      return this.$refs.calendar.getInstance();
+    },
+    ...mapStores(useUserAuthInfoStore)
+  },
+  watch: {
+    selectedView(newView) {
+      this.calendarInstance.changeView(newView);
+      this.setDateRangeText();
+    },
+  },
+  mounted() {
+    this.setDateRangeText();
+    this.getEvents();
+  },
+  methods: {
+    getEvents() {
+        axios.get(`/api/academic-calendar/calendar/${this.$route.params.id}/events`, {
+            headers: {
+                'Authorization': 'Bearer ' + this.userAuthInfoStore.token
+            }
+        })
+        .then( ( res ) => {
+            //console.log(res);
+            res.data.forEach( (event) => {
+                if(event.end_date === null) {
+                    event.end_date = event.start_date;
+                }
+
+                this.events.push({
+                    id: event.id,
+                    calendarId: '0',
+                    title: event.description,
+                    category: 'allday',
+                    start: event.start_date,
+                    end: event.end_date,
+                    label: event.label,
+                    backgroundColor: `#${event.hexadecimal_color}`,
+                    campi: event.campi,
+                    organization: event.organization,
+                    isAllday: true
+                })
+            })
+        })
+    },
+    getTemplateForMilestone(event) {
+        return `<span style="color: #fff; background-color: ${event.backgroundColor};">${event.title}</span>`;
+    },
+    getTemplateForAllday(event) {
+      return `[All day] ${event.title}`;
+    },
+    onSelectDateTime({ start, end }) {
+        this.calendarInstance.clearGridSelections()
+      console.group('onSelectDateTime');
+      console.log(`Date : ${start} ~ ${end}`);
+      console.groupEnd();
+    },
+    onBeforeCreateEvent(eventData) {
+      const event = {
+        calendarId: eventData.calendarId || '',
+        id: String(Math.random()),
+        title: eventData.title,
+        isAllday: eventData.isAllday,
+        start: eventData.start,
+        end: eventData.end,
+        category: eventData.isAllday ? 'allday' : 'time',
+        dueDateClass: '',
+        location: eventData.location,
+        state: eventData.state,
+        isPrivate: eventData.isPrivate,
+      };
+
+      this.calendarInstance.createEvents([event]);
+    },
+    onBeforeUpdateEvent(updateData) {
+      console.group('onBeforeUpdateEvent');
+      console.log(updateData);
+      console.groupEnd();
+
+      const targetEvent = updateData.event;
+      const changes = { ...updateData.changes };
+
+      this.calendarInstance.updateEvent(targetEvent.id, targetEvent.calendarId, changes);
+    },
+
+    onBeforeDeleteEvent({ title, id, calendarId }) {
+      console.group('onBeforeDeleteEvent');
+      console.log('Event Info : ', title);
+      console.groupEnd();
+
+      this.calendarInstance.deleteEvent(id, calendarId);
+    },
+    onAfterRenderEvent({ title }) {
+      console.group('onAfterRenderEvent');
+      console.log('Event Info : ', title);
+      console.groupEnd();
+    },
+    onClickDayName({ date }) {
+      console.group('onClickDayName');
+      console.log('Date : ', date);
+      console.groupEnd();
+    },
+    onClickEvent({ nativeEvent, event }) {
+      console.group('onClickEvent');
+      console.log('MouseEvent : ', nativeEvent);
+      console.log('Event Info : ', event);
+      console.groupEnd();
+    },
+    onClickTimezonesCollapseBtn(timezoneCollapsed) {
+      console.group('onClickTimezonesCollapseBtn');
+      console.log('Is Timezone Collapsed?: ', timezoneCollapsed);
+      console.groupEnd();
+
+      const newTheme = {
+        'week.daygridLeft.width': '100px',
+        'week.timegridLeft.width': '100px',
+      };
+
+      this.calendarInstance.setTheme(newTheme);
+    },
+    onClickTodayButton() {
+      this.calendarInstance.today();
+      this.setDateRangeText();
+    },
+    onClickMoveButton(offset) {
+      this.calendarInstance.move(offset);
+      this.setDateRangeText();
+    },
+    setDateRangeText() {
+      const date = this.calendarInstance.getDate();
+      const start = this.calendarInstance.getDateRangeStart();
+      const end = this.calendarInstance.getDateRangeEnd();
+
+      const startYear = start.getFullYear();
+      const endYear = end.getFullYear();
+
+      switch (this.selectedView) {
+        case 'month':
+          this.dateRangeText = `${date.getFullYear()}.${date.getMonth() + 1}`;
+
+          return;
+        case 'day':
+          this.dateRangeText = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+
+          return;
+        default:
+          this.dateRangeText = `${startYear}.${start.getMonth() + 1}.${start.getDate()} - ${
+            startYear !== endYear ? `${endYear}.` : ''
+          }${end.getMonth() + 1}.${end.getDate()}`;
+      }
+    },
+  },
+  components: {
+    'ToastUICalendar': ToastUICalendar,
+    'BaseButton': BaseButton
+  }
+};
 </script>
