40a5e00ab0d6a1f8cad264fa8a7f9f4c9450b965
Samuel Lima da Silva <samuelima.00@outlook.com>
2024-09-18 20:22:58 -0300

    Ajusta cor e z-index do dropdown menu

--- academic-calendar/src/components/DropdownMenu.vue
+++ academic-calendar/src/components/DropdownMenu.vue
@@ -5,10 +5,15 @@
             
             </slot>
         </div>
-        <ul class="dropdown-menu dropdown-menu-end">
+        <ul class="dropdown-menu dropdown-menu-end bg-secondary">
             <slot name="options">
 
             </slot>
         </ul>
     </div>
 </template>
+<style scoped>
+    .dropdown-menu {
+        z-index: 2000;
+    }
+</style>
